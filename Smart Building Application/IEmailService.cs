bc2ee0d49672d9262a06f648703ca77aaba0c2b9
chirathp <150772146+chirathp@users.noreply.github.com>
2024-05-20 22:56:12 +0530

    IEmailService.cs

---
+++ Smart Building Application/IEmailService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBuilding
+{
+    public interface IEmailService
+    {
+        public void SendMail(string emailAddress, string subject, string message);
+    }
+}
