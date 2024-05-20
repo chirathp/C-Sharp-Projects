e271da80e95489d842a82bfa505815a8a02de53b
chirathp <150772146+chirathp@users.noreply.github.com>
2024-05-20 22:56:39 +0530

    IFireAlarmManager.cs

---
+++ Smart Building Application/IFireAlarmManager.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBuilding
+{
+    public interface IFireAlarmManager : IManager
+    {
+        public void SetAlarm(bool isActive);
+    }
+}
