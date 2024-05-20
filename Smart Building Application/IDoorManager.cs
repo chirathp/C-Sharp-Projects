1ac0c410eee422063547a850de5a293ca8afba12
chirathp <150772146+chirathp@users.noreply.github.com>
2024-05-20 22:55:43 +0530

    IDoorManager.cs

---
+++ Smart Building Application/IDoorManager.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBuilding
+{
+    public interface IDoorManager : IManager
+    {
+        public bool OpenDoor(int doorID);
+        public bool LockDoor(int doorID);
+        public bool OpenAllDoors();
+        public bool LockAllDoors();
+        object OpenDoor(bool v);
+    }
+}
